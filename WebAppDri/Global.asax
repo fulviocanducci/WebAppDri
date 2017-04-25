47197d730b1d182bb2145591759f04019f72c5a9
fulviocanducci <fulviocanducci@hotmail.com>
2017-04-25 20:46:26 -0300

    Add project files.

---
+++ WebAppDri/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppDri.MvcApplication" Language="C#" %>
---
+++ WebAppDri/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppDri.Startup))]
+namespace WebAppDri
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
